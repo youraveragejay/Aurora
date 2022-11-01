a1b0d61083ea5e0837180438bffbcfe8ec221b9d
youraveragejay <jcracknell08@gmail.com>
2022-11-01 18:19:28 +0000

    fixed leaderboard command

---
+++ commands/leaderboard.js
@@ -0,0 +1,54 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { botColour } = require("../data/config");
+const Guild = require("../schemas/guild");
+
+const findMinInArray = (array) => {
+  const min = Math.min(array);
+  console.log(min);
+};
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("leaderboard")
+    .setDescription("View the top 10 users in the server.")
+    .setDMPermission(false),
+  async execute(interaction) {
+    const guildProfile = await Guild.findOne({ guildId: interaction.guild.id });
+    const guild = interaction.guild;
+
+    if (!guildProfile) {
+      await interaction.reply("Unknown guild: " + interaction.guild.name);
+    }
+
+    const leaderboard = {};
+    guildProfile.levels.forEach((val, userId) => {
+      //const user = guild.members.cache.get(key).user;
+      //const username = user.username;
+      const level = val.level;
+      leaderboard[userId] = level;
+    });
+
+    if (leaderboard.length < 0)
+      await interaction.reply({ content: "Error", ephemeral: true });
+
+    const embed = new EmbedBuilder().setColor(botColour);
+
+    const users = Object.keys(leaderboard).map((key) => {
+      return [key, leaderboard[key]];
+    });
+    users.sort((first, second) => {
+      return second[1] - first[1];
+    });
+    users.slice(0, 10);
+
+    users.forEach((user) => {
+      const id = user[0];
+      const level = user[1];
+      const currentUser = interaction.guild.members.cache.get(id).user;
+      const uname = currentUser.username;
+      embed.addFields({ name: `${uname}`, value: `Level ${level}`});
+    });
+
+    await interaction.reply({ embeds: [embed] });
+  },
+};
